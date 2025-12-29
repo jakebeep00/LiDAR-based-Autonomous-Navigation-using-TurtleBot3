4afc2bb1c2dec39479706ba2c3694c354c3d43d0
jakebeep00 <jaegeunkim.work@gmail.com>
2025-12-29 13:37:51 +0900

    Create cartographer_2d.lua

---
+++ config/cartographer/cartographer_2d.lua
@@ -0,0 +1,7 @@
+MAP_BUILDER.use_trajectory_builder_2d = true
+
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.min_range = 0.12
+TRAJECTORY_BUILDER_2D.max_range = 3.5
+
+POSE_GRAPH.optimize_every_n_nodes = 90
